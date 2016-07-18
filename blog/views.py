3767530bf797b0b8e7486b84eb39deda539a28b5
Dragox <879869999@qq.com>
2016-07-18 10:44:52 +0800

    Added view and template for detailed

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
@@ -11,3 +11,9 @@ def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'data': posts})
+
+
+def post_detail(request, pk):
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
