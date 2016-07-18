9a76e4a68a503b78b64fc43cb3dd757ec7794da6
Dragox <879869999@qq.com>
2016-07-18 09:46:17 +0800

    change the HTML for this site

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from blog import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^list/$', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+
+def post_list(request):
+    data = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'data': data})
